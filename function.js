9ee03229d1daa12d31eb4dc4449a1610dd885632
tanjila <tanjilabithi12@gmail.com>
2025-09-18 23:29:02 +0600

    new

--- function.js
+++ function.js
@@ -1,5 +1,28 @@
 function fanOffKor() {
-    concole.log('bosha thake uthe dara');
+    console.log('bosha thake uthe dara');
     console.log('walk toward the switch');
     console.log('click the switch to off of the fan');
 }
+fanOffKor();
+//
+function vatkhao(){
+    console.log('hand wash kore asho');
+    console.log('bosho');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopat gopat kore khao');
+
+}
+vatkhao();
+//
+function am_khao(){
+    console.log('ei nao am khao');
+}
+am_khao();
+//
+function familyMem(){
+    console.log('Tanjila');
+    console.log('Sathy');
+    console.log('Feeha');
+}
+familyMem();
+//
