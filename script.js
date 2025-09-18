f0dca6333bcb155b7833ff194100ed2ad6719fe5
tanjila <tanjilabithi12@gmail.com>
2025-09-18 19:45:37 +0600

    new

--- script.js
+++ script.js
@@ -61,3 +61,67 @@ for(const key of keys2){
     console.log(key, profile2[key]);
 }
 //
+const Books = {
+    Name: 'abc',
+    writer: 'xyz',
+    price: 200
+};
+console.log(Books);
+//
+const article = {
+    title: 'Learning JS',
+    category: 'Programinng'
+};
+if(article.hasOwnProperty('author')){
+    console.log('There is another property.');
+}else{
+    console.log('There is no author property');
+}
+//
+const laptop1 ={
+    brand: 'Dell',
+    model: 'Inspiron',
+    price: 45000
+};
+for(const key in laptop1){
+    const Value = laptop1[key];
+    console.log(key,Value);
+}
+//
+const phone ={
+    brand: 'Samsung',
+    model: 'Galaxy S21',
+    price: 85000
+};
+const keys3 = Object.keys(phone);
+for (const key of keys3){
+    console.log(key, phone[key]);
+};
+//
+const bike = {
+    brand: 'Hero',
+    price: 120000,
+    model: 'Splendor'
+};
+const values3 = Object.values(bike);
+console.log(values3);
+//
+const perSon = {
+    name: 'Tanjila',
+    age:'15',
+    country: 'Bangladesh'
+};
+delete perSon.age;
+console.log(perSon);
+//
+const adminUser = {
+    user: 'admin',
+    email: 'admin@11.com',
+    role: 'superadmin'
+};
+Object.freeze(adminUser);
+//attempt to modify the object
+adminUser.role = 'user';
+adminUser.password = '123456';
+delete adminUser.email;
+console.log(adminUser);
