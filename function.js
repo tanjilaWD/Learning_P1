c34fbbef0e844659daa8423e5c2f9a27c8d79e74
tanjila <tanjilabithi12@gmail.com>
2025-09-20 10:29:18 +0600

    new

--- function.js
+++ function.js
@@ -107,3 +107,43 @@ function tenTimes(number){
 }
 const output = tenTimes(5);
 console.log(output);
+//
+function add(price1, price2){
+    const total = price1 + price2;
+    return total;
+}
+const bill = add(5,80);
+console.log(bill);
+//
+const potato = 35;
+const onion = 62;
+const shirt = 500;
+const pant = 1250;
+function add(price1,price2){
+    return price1 + price2;
+}
+const bill1 = add(potato, onion);
+const bill2 = add(shirt, pant);
+const final = add(bill1, bill2);
+console.log(final);
+//
+function doMath(num1,num2){
+    const sum = num1 + num2;
+    const diff = num1 = num2;
+    const multiply = sum * diff;
+    const result1 = multiply / 2;
+    return result1;
+}
+const result1 = doMath(10,5);
+console.log(result1);
+//
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isEven(5));
+console.log(isEven(110));
+//
